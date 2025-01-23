189a36e6a888f97224c94fe1ca997072109ceec1
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2025-01-23 14:31:55 -0700

    rough draft of the shell

---
+++ src/App.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Deets from "./Deets.svelte";
+</script>
+
+<div id="shell">
+  <main>
+    <div id="left">
+      <div id="placeholder"></div>
+    </div>
+    <div id="right">
+      <h1>Quiz</h1>
+      <hr />
+      <article>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Himenaeos
+          facilisi ultrices curabitur fringilla pretium iaculis ad. Maximus
+          ligula faucibus ex habitant eros. Cubilia id nec tempor mattis tempus
+          est non. Ullamcorper mollis eleifend; taciti netus class platea. Lorem
+          dapibus mauris nostra phasellus platea ultricies ac. Potenti purus
+          vehicula mus ante libero aliquet. Quam velit vitae feugiat viverra
+          congue magnis conubia. Malesuada aenean nec dictumst; aliquam maximus
+          phasellus.
+        </p>
+      </article>
+      <Deets />
+    </div>
+  </main>
+  <nav>
+    <h2>coming soon nav</h2>
+  </nav>
+</div>
+
+<style>
+  #shell {
+    height: 100%;
+    max-height: 100vh;
+    aspect-ratio: 4/3;
+  }
+
+  main {
+    flex: 1;
+    flex-direction: row;
+    padding: 16px;
+    gap: 3%;
+  }
+
+  nav {
+    height: 60px;
+    align-items: center;
+    justify-content: center;
+    color: gray;
+    font-size: 10px;
+  }
+
+  #left {
+    width: 70%;
+  }
+
+  #placeholder {
+    width: 100%;
+    height: 100%;
+    border: 3px solid black;
+    background-color: white;
+    box-shadow: 4px 4px 0px rgba(122, 122, 122, 0.5);
+  }
+
+  #right {
+    padding-top: 10px;
+    flex-direction: column;
+    flex: 1;
+  }
+
+  h1 {
+    font-size: 48px;
+  }
+
+  hr {
+    width: 100%;
+  }
+
+  article {
+    flex: 1;
+  }
+</style>
---
+++ src/Deets.svelte
@@ -0,0 +1,72 @@
+<div class="floating-window">
+  <header>
+    <h3>The-Deets.txt</h3>
+  </header>
+  <div class="row list">
+    <h3>Tools</h3>
+    <ol>
+      <li>Svelte</li>
+      <li>Typescript</li>
+      <li>Vite</li>
+    </ol>
+  </div>
+  <div class="row list">
+    <h3>Features</h3>
+    <ol>
+      <li>Dark Mode</li>
+      <li>Mobile</li>
+      <li>Desktop</li>
+    </ol>
+  </div>
+  <div class="row link">Github</div>
+  <div class="row link">Website</div>
+</div>
+
+<style>
+  .floating-window {
+    background-color: white;
+    box-shadow: 4px 3px 0 1px rgba(70, 70, 70, 0.8);
+    border: 2px solid black;
+  }
+
+  header {
+    font-family: monospace;
+    background-color: #e4dfdf;
+    padding: 12px 8px;
+    border-bottom: 2px solid black;
+    font-size: 12px;
+  }
+
+  .row {
+    padding: 12px 8px;
+    border-bottom: 2px solid black;
+  }
+
+  .list {
+    border-bottom-width: 4px;
+  }
+
+  .list ol,
+  .list li {
+    display: inline;
+    font-family: monospace;
+  }
+
+  .list h3 {
+    margin-bottom: 8px;
+  }
+
+  .list ol {
+    list-style-type: none;
+    font-size: 10px;
+  }
+
+  li + li::before {
+    content: "& ";
+  }
+
+  .link:hover {
+    background-color: #f0f0f0;
+    text-decoration: underline;
+  }
+</style>
